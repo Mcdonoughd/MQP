a63b97db2b667faa9ce4bbc5ddbbd8cbada0aba6
Surya Vadivazhagu <svadivazhagu@wpi.edu>
2020-02-16 13:47:41 -0500

    histogram working, shows histogram in B G R. ready for PR

--- histogram/histogram.py
+++ histogram/histogram.py
@@ -7,13 +7,14 @@ from matplotlib import pyplot as plt
 
 import cv2
 import matplotlib.pyplot as plt
-image = cv2.imread('untouched.jpg')
+image = cv2.imread('individualCellRed.jpg', 1)
 
 for i, col in enumerate(['b', 'g', 'r']):
     hist = cv2.calcHist([image], [i], None, [256], [0, 256])
-    plt.plot(hist, color='green')
+    plt.plot(hist, color=col)
     plt.xlim([0, 256])
-    plt.ylim([0, 65000])
+
+plt.show()
 
 
 
